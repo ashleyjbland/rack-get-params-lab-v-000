063ba3a39c3c4828777f53ee3be163b836a78446
Ashley Bland <ashleyjbland@gmail.com>
2019-03-16 20:05:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,8 +18,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
-      @@cart.each do |cart_item|
-        resp.write "#{cart_item}\n"
+        @@cart.each {|cart_item| resp.write "#{cart_item}\n"}
       end
     end
     resp.finish
